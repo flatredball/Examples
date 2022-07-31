f38786e759154bc596161e3d0f9fbc7263c92016
Justin Johnson <justin.d.johnson@gmail.com>
2022-07-31 14:41:38 -0600

    Treeview kinda works. Second-level items are broken.

--- NewGum/NewGum/Screens/DemoScreen.cs
+++ NewGum/NewGum/Screens/DemoScreen.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.IO;
 
 using FlatRedBall;
 using FlatRedBall.Input;
@@ -13,8 +14,8 @@ using FlatRedBall.Math;
 using FlatRedBall.Math.Geometry;
 using FlatRedBall.Localization;
 using Microsoft.Xna.Framework;
-
-
+using System.Reflection;
+using FlatRedBall.Forms.Controls;
 
 namespace NewGum.Screens
 {
@@ -23,22 +24,11 @@ namespace NewGum.Screens
 
         void CustomInitialize()
         {
-            Forms.ResolutionBox.Items.Add("320x240");
-            Forms.ResolutionBox.Items.Add("480x360");
-            Forms.ResolutionBox.Items.Add("600x800");
-            Forms.ResolutionBox.Items.Add("1024x768");
 
-            Forms.DetectResolutionsButton.Click += (not, used) =>
-            {
-                Forms.ResolutionBox.Items.Add("Some Resolution");
-            };
 
-            Forms.ComboBoxInstance.Items.Add("Creative");
-            Forms.ComboBoxInstance.Items.Add("Easy");
-            Forms.ComboBoxInstance.Items.Add("Medium");
-            Forms.ComboBoxInstance.Items.Add("Hard");
-            Forms.ComboBoxInstance.Items.Add("Permadeath");
-            Forms.ComboBoxInstance.Items.Add("Extreme");
+            PopulateListBox();
+            PopulateComboBox();
+            PopulateTreeView();
         }
 
         void CustomActivity(bool firstTimeCalled)
@@ -59,5 +49,53 @@ namespace NewGum.Screens
 
         }
 
+        void PopulateListBox()
+        {
+            Forms.ResolutionBox.Items.Add("320x240");
+            Forms.ResolutionBox.Items.Add("480x360");
+            Forms.ResolutionBox.Items.Add("600x800");
+            Forms.ResolutionBox.Items.Add("1024x768");
+
+            Forms.DetectResolutionsButton.Click += (not, used) =>
+            {
+                Forms.ResolutionBox.Items.Add("Some Resolution");
+            };
+        }
+
+        void PopulateComboBox()
+        {
+            Forms.ComboBoxInstance.Items.Add("Creative");
+            Forms.ComboBoxInstance.Items.Add("Easy");
+            Forms.ComboBoxInstance.Items.Add("Medium");
+            Forms.ComboBoxInstance.Items.Add("Hard");
+            Forms.ComboBoxInstance.Items.Add("Permadeath");
+            Forms.ComboBoxInstance.Items.Add("Extreme");
+        }
+
+        void PopulateTreeView()
+        {
+            PopulateTreeViewRecursively(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+        }
+
+        void PopulateTreeViewRecursively(string path)
+        {
+            var treeViewItem = new TreeViewItem();
+            treeViewItem.UpdateToObject(Path.GetFileName(path));
+
+            // get dirs
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                PopulateTreeViewRecursively(dir);
+            }
+
+            // get files
+            foreach (var file in Directory.GetFiles(path))
+            {
+                var name = Path.GetFileName(file);
+                treeViewItem.Items.Add(name);
+            }
+
+            Forms.TreeViewInstance.Items.Add(treeViewItem);
+        }
     }
 }
