7f407ef482e2edee6c77d14d3f1249f2947b2654
Victor Chelaru <VicChelaru@gmail.com>
2022-08-01 09:25:18 -0600

    Fixed TextInstance names on RadioButton and CheckBox.

--- NewGum/NewGum/Forms/UserControls/CodeOnlyUserControl.cs
+++ NewGum/NewGum/Forms/UserControls/CodeOnlyUserControl.cs
@@ -16,34 +16,73 @@ namespace NewGum.Forms.UserControls
 
             this.Visual.HeightUnits = Gum.DataTypes.DimensionUnitType.RelativeToChildren;
 
-            var stackLayout = new StackPanel();
-            stackLayout.X = 8;
-            stackLayout.Y = 8;
-            stackLayout.Width = -16;
-            stackLayout.Visual.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToContainer;
-            this.AddChild(stackLayout);
+            var stackPanel = new StackPanel();
+            stackPanel.X = 8;
+            stackPanel.Y = 8;
+            stackPanel.Width = -16;
+            stackPanel.Visual.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToContainer;
+            this.AddChild(stackPanel);
 
             var label = new Label();
             label.Text = "This is a form made purely in code";
             label.Width = 0;
             label.Visual.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToContainer;
-            stackLayout.AddChild(label);
+            stackPanel.AddChild(label);
 
-            var nameLabel = new Label();
-            nameLabel.Y = 5;
-            nameLabel.Text = "Enter name:";
-            stackLayout.AddChild(nameLabel);
+            AddTextBoxWithLabel("Name");
+            AddTextBoxWithLabel("Age");
+            AddTextBoxWithLabel("Address");
+            AddTextBoxWithLabel("Something Else");
 
-            var textBox = new TextBox();
-            textBox.Placeholder = "Name";
-            stackLayout.AddChild(textBox);
+            AddCheckBox("Checkbox Option 1");
+            AddCheckBox("Checkbox Option 2");
+            AddCheckBox("Checkbox Option 3");
+            AddCheckBox("Checkbox Option 4");
 
+            var groupBox = new StackPanel();
+            stackPanel.AddChild(groupBox);
 
+            AddRadioButton("Radio Button Option 1");
+            AddRadioButton("Radio Button Option 2");
+            AddRadioButton("Radio Button Option 3");
+            AddRadioButton("Radio Button Option 4");
+            AddRadioButton("Radio Button Option 5");
 
+            for (int i = 0; i < 5; i++)
+            {
+                var button = new Button();
+                button.Text = "Button " + i;
+                stackPanel.AddChild(button);
 
-            var button = new Button();
-            button.Text = "OK";
-            stackLayout.AddChild(button);
+            }
+
+            void AddTextBoxWithLabel(string labelText)
+            {
+                var nameLabel = new Label();
+                nameLabel.Y = 5;
+                nameLabel.Text = labelText;
+                stackPanel.AddChild(nameLabel);
+
+                var textBox = new TextBox();
+                //textBox.Placeholder = "Name";
+                stackPanel.AddChild(textBox);
+            }
+
+            void AddCheckBox(string text)
+            {
+                var checkBox = new CheckBox();
+                checkBox.Text = text;
+                stackPanel.AddChild(checkBox);
+            }
+
+            void AddRadioButton(string text)
+            {
+                var radioButton = new RadioButton();
+                radioButton.Text = text;
+                radioButton.Width = 200;
+                stackPanel.AddChild(radioButton);
+            }
         }
+
     }
 }
