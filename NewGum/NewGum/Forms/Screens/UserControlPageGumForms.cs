56c8b4d80922301e6713d9acb52d4a0d0fce43b6
Victor Chelaru <VicChelaru@gmail.com>
2022-08-01 08:12:50 -0600

    Lots more work on default project.

---
+++ NewGum/NewGum/Forms/Screens/UserControlPageGumForms.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewGum.FormsControls.Screens
+{
+    public partial class UserControlPageGumForms
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
---
+++ NewGum/NewGum/Forms/UserControls/CodeOnlyUserControl.cs
@@ -0,0 +1,49 @@
+﻿using FlatRedBall.Forms.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewGum.Forms.UserControls
+{
+    public class CodeOnlyUserControl : UserControl
+    {
+        public CodeOnlyUserControl() : base()
+        {
+            this.Width = 300;
+            this.Height = 400;
+
+            this.Visual.HeightUnits = Gum.DataTypes.DimensionUnitType.RelativeToChildren;
+
+            var stackLayout = new StackPanel();
+            stackLayout.X = 8;
+            stackLayout.Y = 8;
+            stackLayout.Width = -16;
+            stackLayout.Visual.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToContainer;
+            this.AddChild(stackLayout);
+
+            var label = new Label();
+            label.Text = "This is a form made purely in code";
+            label.Width = 0;
+            label.Visual.WidthUnits = Gum.DataTypes.DimensionUnitType.RelativeToContainer;
+            stackLayout.AddChild(label);
+
+            var nameLabel = new Label();
+            nameLabel.Y = 5;
+            nameLabel.Text = "Enter name:";
+            stackLayout.AddChild(nameLabel);
+
+            var textBox = new TextBox();
+            textBox.Placeholder = "Name";
+            stackLayout.AddChild(textBox);
+
+
+
+
+            var button = new Button();
+            button.Text = "OK";
+            stackLayout.AddChild(button);
+        }
+    }
+}
